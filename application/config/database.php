7e6ee7119d511c64a305370be624a4b033cfc43b
Uğur Burak TENDÜRÜS <utendurus@gmail.com>
2013-11-07 21:35:55 +0200

    veritabanı deneme olarak değiştirildi

--- application/config/database.php
+++ application/config/database.php
@@ -51,7 +51,7 @@ $active_record = TRUE;
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = '';
-$db['default']['database'] = 'gardrobe';
+$db['default']['database'] = 'deneme';
 
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
